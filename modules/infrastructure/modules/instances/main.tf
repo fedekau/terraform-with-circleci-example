875173ec54b93ff8610c37fad2be8c4ee0074a36
fedekau <fede.kau@gmail.com>
2018-03-22 19:43:24 -0300

    Install mysql-client on instances
    I KNOW THAT I HAVE COMMITED A PRIVATE KEY. I DID IT SO IT IS EASIER FOR
    THE READER TO SEE HOW ALL WORKS.

--- modules/infrastructure/modules/instances/main.tf
+++ modules/infrastructure/modules/instances/main.tf
@@ -18,17 +18,33 @@ resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
 
-  subnet_id              = "${var.public-a-subnet-id}"
-  vpc_security_group_ids = ["${aws_security_group.allow-external-ssh.id}"]
+  subnet_id = "${var.public-a-subnet-id}"
 
-  key_name = "${aws_key_pair.id_rsa_kauffman_federico.key_name}"
+  vpc_security_group_ids = [
+    "${aws_security_group.allow-external-ssh.id}",
+    "${aws_security_group.allow-all-http-outgoing.id}",
+  ]
+
+  key_name = "${aws_key_pair.id_dummy.key_name}"
+
+  provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = "${file("../../.keys/id_dummy")}"
+    }
+
+    inline = [
+      "sudo apt-get install mysql-client -y",
+    ]
+  }
 
   tags {
     environment = "${var.environment}"
   }
 }
 
-resource "aws_key_pair" "id_rsa_kauffman_federico" {
-  key_name   = "id_rsa_kauffman_federico"
-  public_key = "${file("~/.ssh/id_rsa_kauffman_federico.pub")}"
+resource "aws_key_pair" "id_dummy" {
+  key_name   = "id_dummy"
+  public_key = "${file("../../.keys/id_dummy.pub")}"
 }
--- modules/infrastructure/modules/instances/security_groups.tf
+++ modules/infrastructure/modules/instances/security_groups.tf
@@ -10,3 +10,23 @@ resource "aws_security_group" "allow-external-ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "allow-all-http-outgoing" {
+  name        = "allow-all-http-outgoing"
+  description = "Allow outgoing http connections to the world."
+  vpc_id      = "${var.vpc-id}"
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
