0654e8bcce13aa5515274b3f1b546642a19a6bbd
Eric Garza <garzparz@gmail.com>
2019-03-28 19:11:59 -0700

    Update outputs.tf
    [ci skip]

--- environments/staging/outputs.tf
+++ environments/staging/outputs.tf
@@ -1,1 +1,7 @@
+output "web-alb-dns-name" {
+  value = "${module.staging-infrastructure.web-alb-dns-name}"
+}
 
+output "web-instance-ips" {
+  value = "${module.staging-infrastructure.web-instance-ips}"
+}
