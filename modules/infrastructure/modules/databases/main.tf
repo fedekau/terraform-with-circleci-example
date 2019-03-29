a6809b9d7cda98d00955d9679d73af3c052f4b52
Eric Garza <garzparz@gmail.com>
2019-03-28 21:06:15 -0700

    Update main.tf

--- modules/infrastructure/modules/databases/main.tf
+++ modules/infrastructure/modules/databases/main.tf
@@ -14,7 +14,7 @@ resource "aws_db_instance" "master" {
   engine               = "mysql"
   engine_version       = "5.7"
   instance_class       = "db.t2.micro"
-  name                 = "${var.prefix}-${var.environment}"
+  name                 = "${var.environment}"
   username             = "username"
   password             = "password"
   db_subnet_group_name = "${aws_db_subnet_group.rds-private-subnet-group.name}"
