218d777e6fdd1f07decc84b07cac8a364b0d12cd
Eric Garza <garzparz@gmail.com>
2019-03-28 20:51:02 -0700

    Update main.tf

--- modules/state/main.tf
+++ modules/state/main.tf
@@ -1,17 +1,3 @@
-resource "aws_s3_bucket" "state-file-bucket" {
-  bucket = "${var.environment}-state-terraform-circleci-lab"
-
-  versioning {
-    enabled = true
-  }
-
-  tags {
-    env = "${var.environment}"
-    dept = "git"
-    app  = "terraform-circleci-lab"
-  }
-}
-
 resource "aws_dynamodb_table" "state-file-locking-table" {
   name           = "${var.prefix}-${var.environment}-state-file-locking"
   hash_key       = "LockID"
