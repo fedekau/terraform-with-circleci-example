9f7339a2a47ff52850a6c4382466c4406c248f60
Eric Garza <garzparz@gmail.com>
2019-03-28 17:27:04 -0700

    Update main.tf

--- environments/production/main.tf
+++ environments/production/main.tf
@@ -12,7 +12,7 @@ terraform {
   backend "s3" {
     bucket  = "production-state-file"
     key     = "terraform.tfstate"
-    region  = "us-east-1"
+    region  = "us-east-2"
     encrypt = true
   }
 }
