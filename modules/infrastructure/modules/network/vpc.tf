2e848ee25d7748704ddcca53299c14aeb0a5309a
Eric Garza <garzparz@gmail.com>
2019-03-28 23:07:29 -0700

    Update vpc.tf
    [ci skip]

--- modules/infrastructure/modules/network/vpc.tf
+++ modules/infrastructure/modules/network/vpc.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
   tags {
-    name        = "${var.prefix}-main"
+    Name        = "${var.prefix}-main"
     environment = "${var.environment}"
   }
 }
